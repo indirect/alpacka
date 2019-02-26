ff0f572ae545695342e1e6a4dd56bccefc816dd0
Andre Arko <andre@arko.net>
2019-02-25 19:42:56 -0800

    skeleton tab-based app

---
+++ Alpacka/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Alpacka
+//
+//  Created by André Arko on 2/25/19.
+//  Copyright © 2019 Cuti.es. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ Alpacka/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Alpacka
+//
+//  Created by André Arko on 2/25/19.
+//  Copyright © 2019 Cuti.es. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
