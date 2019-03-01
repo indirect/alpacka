517e466ac9463ba61c3042f009191b7420a0e5ec
Andre Arko <andre@arko.net>
2019-03-01 16:13:56 +0700

    maybe just a navigator to a table view

---
+++ Alpacka/Trip.swift
@@ -0,0 +1,19 @@
+//
+//  Trip.swift
+//  Alpacka
+//
+//  Created by André Arko on 3/1/19.
+//  Copyright © 2019 Cuti.es. All rights reserved.
+//
+
+import Foundation
+
+class Trip {
+    var name: String
+    var location: String
+    
+    init(name: String, location: String) {
+        self.name = name
+        self.location = location
+    }
+}
